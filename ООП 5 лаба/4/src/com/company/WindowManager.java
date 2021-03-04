c8af65ad5cf43ecabdb5b655bdf98b51c7c44b4b
MAL1S <MAL1S@users.noreply.github.com>
2021-03-04 12:19:38 +0800

    added addListener and table organization

--- ООП 5 лаба/4/src/com/company/WindowManager.java
+++ ООП 5 лаба/4/src/com/company/WindowManager.java
@@ -2,11 +2,22 @@ package com.company;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
 
 
 public class WindowManager extends JFrame {
+    JTable table;
+    String[] columnNames = {"Номер зачетки", "ФИО", "Шифр группы", "Дата поступления", "Номер телефона", "Дата рождения"};
+    JFrame frame;
+    JPanel panel, rightPanel;
+    JButton addButton, removeButton, editButton, button4, button5, button6;
+    JScrollPane scrollPane;
 
     public WindowManager() {
         /*setSize(800, 600);
@@ -32,38 +43,30 @@ public class WindowManager extends JFrame {
         panel.add(button2);
         add(panel, BorderLayout.EAST);*/
 
-        String[][] data = {
-                {"Карбушев Иван Владимирович\n",
-                        "Зачет\nЭкзамен",
-                        "ООП\nТехнологии программирования",
-                        "Аршинский В.Л.\nКаташевцев А.А.",
-                        "21.12.2020\n24.12.2020",
-                        "9:00\n10:00",
-                        "Зачет\n5"}
-        };
 
-        String[] columnNames = {"ФИО", "Формат", "Предмет", "Преподаватель", "Дата", "Время", "Оценка"};
 
-        JTable table = new JTable(data, columnNames);
+        table = new JTable(new String[][]{}, columnNames);
 
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Test frame");
+        //JFrame.setDefaultLookAndFeelDecorated(true);
+        frame = new JFrame("Test frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //объявление панелей и их лейаутов
-        JPanel panel = new JPanel();
-        JPanel rightPanel = new JPanel();
+        panel = new JPanel();
+        rightPanel = new JPanel();
         rightPanel.setLayout(new GridLayout(6, 1));
         panel.setLayout(new BorderLayout());
 
         //объявление кнопок
-        JButton addButton = new JButton("Add");
-        addButton.setPreferredSize(new Dimension(125, 50));
-        JButton removeButton = new JButton("Remove");
-        JButton editButton = new JButton("Edit");
-        JButton button4 = new JButton("...");
-        JButton button5 = new JButton("...");
-        JButton button6 = new JButton("...");
+        addButton = new JButton("Add");
+        addButton.setPreferredSize(new Dimension(121, 50));
+        removeButton = new JButton("Remove");
+        editButton = new JButton("Edit");
+        button4 = new JButton("...");
+        button5 = new JButton("...");
+        button6 = new JButton("...");
+
+        addButton.addActionListener(new addButtonListener());
 
         //добавление кнопок
         rightPanel.add(addButton);
@@ -74,26 +77,66 @@ public class WindowManager extends JFrame {
         rightPanel.add(button6);
 
         //таблица
-
-        table.getColumnModel().getColumn(0).setCellRenderer(new DefaultTableCellRenderer());
-        //table.getColumnModel().getColumn(1).setCellRenderer(new TextAreaRenderer());
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        table.getColumnModel().getColumn(0).setPreferredWidth(200);
-        table.getColumnModel().getColumn(2).setPreferredWidth(100);
-        table.getColumnModel().getColumn(3).setPreferredWidth(120);
+        table.getColumnModel().getColumn(1).setMaxWidth(Integer.MAX_VALUE);
+        table.getColumnModel().getColumn(0).setMinWidth(130);
+        table.getColumnModel().getColumn(1).setMinWidth(200);
+        table.getColumnModel().getColumn(2).setMinWidth(110);
+        table.getColumnModel().getColumn(3).setMinWidth(110);
+        table.getColumnModel().getColumn(4).setMinWidth(110);
+        table.getColumnModel().getColumn(5).setMinWidth(100);
         table.setFillsViewportHeight(true);
-        JScrollPane scrollPane = new JScrollPane(table);
+
+        scrollPane = new JScrollPane(table);
 
         //добавление контента в панели
         panel.add(scrollPane, BorderLayout.CENTER);
         panel.add(rightPanel, BorderLayout.EAST);
 
         frame.getContentPane().add(panel);
-        frame.setPreferredSize(new Dimension(858, 600));
+        frame.setPreferredSize(new Dimension(900, 600));
         frame.pack();
         frame.setVisible(true);
         //почему то, выдает много ошибок
-        //frame.setResizable(false);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
     }
+
+    private void addRow(Vector<String> values) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.addRow(values);
+    }
+
+    private class addButtonListener extends JFrame implements ActionListener {
+        private Dialog dialog;
+        private JTextField personalId, name, groupId, enrolDate, mobileNumber, birthDate;
+        JPanel addPanel;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            addPanel = new JPanel();
+            addPanel.setLayout(new BoxLayout(addPanel, BoxLayout.Y_AXIS));
+
+            //поля ввода
+            personalId = new JTextField(10);
+            name = new JTextField(40);
+            groupId = new JTextField(10);
+            enrolDate = new JTextField(8);
+            mobileNumber = new JTextField(11);
+            birthDate = new JTextField(8);
+
+            addPanel.add(personalId);
+            addPanel.add(name);
+            addPanel.add(groupId);
+            addPanel.add(enrolDate);
+            addPanel.add(mobileNumber);
+            addPanel.add(birthDate);
+
+            setContentPane(addPanel);
+            setSize(300, 300);
+            setVisible(true);
+        }
+    }
 }
+
+//http://java-online.ru/swing-jtextfield.xhtml
